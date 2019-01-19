6b2c13a0b5904a177d125dc0afd5900a165548fb
Timothy Brathwaite <timothyb0912@gmail.com>
2019-01-19 15:51:26 -0800

    Added first three of seven viz functions

--- pylogit/viz/__init__.py
+++ pylogit/viz/__init__.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
 """
 Visualization module for PyLogit.
 """
 from __future__ import absolute_import
 
 from .utils import progress, is_categorical, simulate_choice_vector
+from .sim_cdf import plot_simulated_cdf_traces
+from .sim_kde import plot_simulated_kde_traces
+from .cont_scalars import plot_continous_scalars
