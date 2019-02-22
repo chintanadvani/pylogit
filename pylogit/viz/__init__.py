383f7373d04031476ef61a2be68dd435c680790a
Timothy Brathwaite <timothyb0912@gmail.com>
2019-02-22 09:42:10 -0800

    Shortened function names for sim_kdes and sim_cdfs.

--- pylogit/viz/__init__.py
+++ pylogit/viz/__init__.py
@@ -5,8 +5,8 @@ Visualization module for PyLogit.
 from __future__ import absolute_import
 
 from .utils import progress, is_categorical, simulate_choice_vector
-from .sim_cdf import plot_simulated_cdf_traces
-from .sim_kde import plot_simulated_kde_traces
+from .sim_cdf import plot_simulated_cdfs
+from .sim_kde import plot_simulated_kdes
 from .cont_scalars import plot_continous_scalars
 from .disc_scalars import plot_discrete_scalars
 from .market import plot_simulated_market_shares
