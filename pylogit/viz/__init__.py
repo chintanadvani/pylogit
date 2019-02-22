8534fac12372e1a8cadce93e212756c4660917ca
Timothy Brathwaite <timothyb0912@gmail.com>
2019-02-22 09:37:13 -0800

    Making binned reliability accessible from main viz module.

--- pylogit/viz/__init__.py
+++ pylogit/viz/__init__.py
@@ -10,3 +10,4 @@ from .sim_kde import plot_simulated_kde_traces
 from .cont_scalars import plot_continous_scalars
 from .disc_scalars import plot_discrete_scalars
 from .market import plot_simulated_market_shares
+from .reliability import plot_binned_reliability
