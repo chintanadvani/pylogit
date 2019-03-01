396b5e355cff3d2ea27789cf11340fa44fe82776
Timothy Brathwaite <timothyb0912@gmail.com>
2019-02-28 23:00:46 -0800

    Refactoring binning functions to plot_utils.

--- pylogit/viz/reliability.py
+++ pylogit/viz/reliability.py
@@ -11,104 +11,11 @@ import matplotlib.pyplot as plt
 
 from .utils import progress
 from .plot_utils import _label_despine_save_and_show_plot
+from .plot_utils import _determine_bin_obs
+from .plot_utils import _plot_single_binned_x_vs_binned_y
 
 # Set the plotting style
 sbn.set_style('darkgrid')
-
-
-def _determine_bin_obs(total, partitions):
-    """
-    Determines the number of observations that should be in a given partition.
-
-    Parameters
-    ----------
-    total : positive int.
-        Denotes the total number of observations that are to be partitioned.
-    partitions : positive int.
-        Denotes the number of partitions that are to be created. Should be
-        less than or equal to `total`.
-
-    Returns
-    -------
-    obs_per_partition : 1D ndarray of positive its.
-        Denotes the number of observations to be placed in each partition.
-        Will have one element per partition.
-    """
-    partitions_float = float(partitions)
-    naive = int(total / partitions_float) * np.ones(partitions)
-    correction = np.ones(partitions)
-    correction[total % partitions:] = 0
-    return (naive + correction).astype(int)
-
-
-def _populate_bin_means_for_plots(x_vals,
-                                  y_vals,
-                                  obs_per_bin,
-                                  mean_x,
-                                  mean_y,
-                                  auxillary_y=None,
-                                  auxillary_mean=None):
-    """
-    Populate the mean per bin of predicted probabilities, observed outcomes,
-    and simulated outcomes.
-
-    Parameters
-    ----------
-    x_vals : 1D ndarray of floats.
-        Elements should be the sorted values to be placed on the x-axis.
-    y_vals : 1D ndarray of floats.
-        Elements should be the values to be averaged and placed on the y-axis.
-        Should have been sorted in the same order as `x_vals`.
-    obs_per_bin : 1D ndarray of positive ints.
-        There should be one element per bin. Each element should denote the
-        number of observations to be used in each partition.
-    mean_x, mean_y : 1D ndarray.
-        `mean_x.size` and `mean_y.size` should equal `obs_per_bin.size`.
-    auxillary_y : 1D ndarray or None, optional.
-        Same as `y_vals` except these elements denote additional values to be
-        plotted on the y-axis.
-    auxillary_mean : 1D ndarray or None, optional.
-        Same as `mean_probs` and `mean_obs`.
-
-    Returns
-    -------
-    mean_x : 1D ndarray.
-        Will have 1 element per partition. Each value will denote the mean of
-        the `x_vals` for all observations in the partition.
-    mean_y : 1D ndarray.
-        Will have 1 element per partition. Each value will denote the mean of
-        the `y_vals` for all observations in the partition.
-    auxillary_mean : 1D ndarray or None.
-        Will have 1 element per partition. Each value will denote the mean of
-        the `auxillary_y` for all observations in the partition. If
-        `auxillary_mean` was passed as None, it will be returned as None.
-    """
-    # Initialize a row counter
-    row_counter = 0
-
-    # Iterate over each of the partitions
-    for i in range(obs_per_bin.size):
-        # Get the upper and lower ranges of the slice
-        lower_row = row_counter
-        upper_row = row_counter + obs_per_bin[i]
-
-        # Get the particular observations we care about
-        rel_x = x_vals[lower_row:upper_row]
-        rel_y = y_vals[lower_row:upper_row]
-
-        # Store the mean probs and mean y
-        mean_x[i] = rel_x.mean()
-        mean_y[i] = rel_y.mean()
-
-        # Store the mean simulated y per group
-        if auxillary_y is not None:
-            rel_auxillary_y = auxillary_y[lower_row:upper_row]
-            auxillary_mean[i] = rel_auxillary_y.mean()
-
-        # Update the row counter
-        row_counter += obs_per_bin[i]
-
-    return mean_x, mean_y, auxillary_mean
 
 
 def _check_reliability_args(probs, choices, partitions, sim_y):
@@ -197,23 +104,16 @@ def _plot_single_binned_reliability_curve(probs,
     current_probs = current_probs[sort_order]
     current_choices = choices[sort_order]
 
-    # Populate the bin means of predicted probabilities,
-    # observed choices, and simulated choices
-    population_results =\
-        _populate_bin_means_for_plots(
-            current_probs, current_choices,
-            obs_per_partition, mean_probs_per_group, mean_y_per_group)
-    mean_probs_per_group = population_results[0]
-    mean_y_per_group = population_results[1]
-
-    # Plot the mean predicted probs per group versus
-    # the mean observations per group
-    ax.plot(mean_probs_per_group,
-            mean_y_per_group,
-            c=line_color,
-            alpha=alpha,
-            label=current_line_label)
-    return
+    _plot_single_binned_x_vs_binned_y(current_probs,
+                                      current_choices,
+                                      ax,
+                                      current_line_label,
+                                      line_color,
+                                      alpha,
+                                      obs_per_partition,
+                                      mean_probs_per_group,
+                                      mean_y_per_group)
+    return None
 
 
 def add_ref_line(ax, ref_label="Perfect Calibration"):
@@ -249,7 +149,7 @@ def plot_binned_reliability(probs,
                             alpha=None,
                             sim_y=None,
                             sim_line_color='#a6cee3',
-                            sim_label=None,
+                            sim_label='Simulated vs Predicted',
                             sim_alpha=0.5,
                             x_label='Mean Predicted Probability',
                             y_label='Binned\nEmpirical\nProbability',
@@ -286,7 +186,7 @@ def plot_binned_reliability(probs,
         probabilities and the binned, empirical probabilities. Default is
         'Observed vs Predicted'.
     alpha : positive float in [0.0, 1.0], or `None`, optional.
-        Determines the opacity of the elements drawn on the plot.
+        Determines the opacity of the observed data drawn on the plot.
         0.0 == transparent and 1.0 == opaque. Default == 1.0.
     sim_y : 2D ndarray or None, optional.
         Denotes the choices that were simulated based on `probs`. If passed,
@@ -301,7 +201,7 @@ def plot_binned_reliability(probs,
     sim_line_label : str, or None, optional.
         Denotes the label to be used for the lines relating the predicted
         probabilities and the binned, empirical probabilities based on the
-        simulated choices. Default is None.
+        simulated choices. Default is 'Simulated vs Predicted'.
     sim_alpha : positive float in [0.0, 1.0], or `None`, optional.
         Determines the opacity of the simulated reliability curves.
         0.0 == transparent and 1.0 == opaque. Default == 0.5.
